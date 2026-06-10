5df9b9531944d2506b45d426c433e80daf9e5f96
SrDiegoH <diegohenrique4222@gmail.com>
2026-06-10 16:08:22 -0300

    feat: get refined info

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
+import json
+
 from fastapi import FastAPI
 from playwright.async_api import async_playwright
 
+_MOTORCYCLES = {
+    'R3 ABS 70th': '30176', 
+    'FACTOR 150 UBS 2016 A 2024': '30093', 
+    'FZ15 2023 A 2024': '30094', 
+    'FAZER 250 2018 A 2024': '30095', 
+    'CROSSER XTZ150 Z 2015 A 2024': '30097', 
+    'CROSSER XTZ150 S 2015 A 2024': '30096', 
+    'XMAX 2021 A 2024': '30098', 
+    'NMAX CONNECTED 2023 A 2024': '30101', 
+    'LANDER 250 2020 A 2024': '30105', 
+    'FACTOR 125 i (UBS) 2017 A 2025': '30106', 
+    'FAZER 150 UBS 2014 A 2025': '30107', 
+    'FLUO ABS 2022 A 2025': '30108', 
+    'NEO 125 2017 A 2024': '30115', 
+    'MT03 ABS 2021 A 2025': '30121', 
+    'NOVA NMAX CONNECTED 2025': '30127', 
+    'XMAX CONNECTED 2025': '30123', 
+    'LANDER 250 ABS CONNECTED 2025': '30126', 
+    'CROSSER XTZ150 S 2025': '30125', 
+    'CROSSER XTZ150 Z 2025': '30131', 
+    'YZF-R3 ABS 2020 A 2025': '30109', 
+    'FACTOR': '30129', 
+    'NOVA FACTOR DX 2025': '30132', 
+    'FLUO ABS HYBRID CONNECTED': '30171', 
+    "NEO'S DUAL CONNECTED 2026": '30130', 
+    'YZF R3 ABS CONNECTED 2026': '30140', 
+    'NOVA MT-03 CONNECTED 2026': '30141', 
+    'NOVA MT-07 CONNECTED 2026': '30142', 
+    'NOVA XMAX 300 CONNECTED': '30147', 
+    'TÉNÉRÉ 700': '30143', 
+    'AEROX ABS CONNECTED': '30164', 
+    'FAZER FZ15 ABS CONNECTED 2025 A 2026': '30160', 
+    'R15 ABS 2024 A 2026': '30163', 
+    'FZ25 CONNECTED DE 2025 A 2026': '30161', 
+    'LANDER CONNECTED 2025 a 2026': '30167', 
+    'NOVA NMAX ABS CONNECTED': '30168', 
+    'FAZER FZ25 CONNECTED': '30173', 
+    'YAMAHA ZR HYBRID CONNECTED': '30175', 
+    'Nova Factor 2025 a 2026': '30174', 
+    'NOVA FACTOR DX 2025 a 2026': '30172', 
+    'CROSSER 150 Z ABS 2025 a 2026': '30170', 
+    'CROSSER 150 S ABS 2025 a 2026': '30169'
+}
+
 app = FastAPI()
 
-@app.get('/')
-async def rodar_automacao():
-    ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36 OPR/132.0.0.0'
+_get_revision_data = lambda revision: {
+    'revisionByKm': revision.revisionByKm,
+    'revisionByMonth': revision.revisionByMonth,
+    'revisionPriceInCash': revision.priceInCash
+}
+
+def _refine_data(data):
+    content = data.split('1:')[1]
+
+    revisions = json.loads(content).result.items[0].revisions
+
+    return list(map(_get_revision_data, revisions))
+
+async def _get_yamaha_data(name):
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36 OPR/132.0.0.0'
     domain = 'www.yamaha-motor.com.br'
 
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True)
-        context = await browser.new_context(user_agent=ua)
+    async with async_playwright() as playwrite:
+        browser = await playwrite.chromium.launch(headless=True)
+        context = await browser.new_context(user_agent=user_agent)
 
         await context.add_cookies([
             {
@@ -28,6 +86,7 @@ async def rodar_automacao():
         ])
 
         page = await context.new_page()
+
         await page.goto(f'https://www.yamaha-motor.com.br')
 
         headers = {
@@ -53,10 +112,20 @@ async def rodar_automacao():
         response = await page.request.post(
             f'https://www.yamaha-motor.com.br/servicos',
             headers=headers,
-            data='[{"productIds":["30095"]}]'
+            data=f'[{{"productIds":["{_MOTORCYCLES[name]}"]}}]'
         )
 
-        texto_resposta = await response.text()
+        response_text = await response.text()
         await browser.close()
 
-        return {'status': response.status, 'data': texto_resposta}
+        return response.status, response_text
+
+@app.get('/')
+async def index(name):
+    if not name:
+        return None
+
+    status, response = _get_yamaha_data(name)
+
+    if status == 200:
+        return _refine_data(response)
